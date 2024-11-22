79898a4fb590de9ac48e918d14dedc933f8c80b9
khnguyenn <khoinguyennn231@gmail.com>
2024-11-22 15:34:51 +0700

    Update Foward and Back

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # DigitRecognition
+
+The Handwritten Digit Recognition is a project developed to classify digits drawn by the user in an interface.
+
+
+### The Data Set 
+The Data Set : MNIST 
+
+The Training and Calculations of The Algorithm were implemented by Numpy Library.
+
+The main objective of this project is to gain an in-depth understanding of how a neural network functions.
+
+## How it works
+### Algorithm
+The algorithm is a `neural network` with dense layers, following this configuration:
+
+- **Input Size**: 784   
+- **Hidden Layer 1**: 50 neurons, activation function **ReLU**  
+- **Hidden Layer 2**: 20 neurons, activation function **ReLU**  
+- **Output Layer**: 10 neurons, activation function **Softmax**
+
+**Backpropagation**: The `batch gradient descent` method was used with a batch size of 40 and 100 epochs, with a learning rate of 0.005.
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import numpy as np 
+# Load the DataSet
 
 
 # Implementation of Activation Function 
@@ -21,8 +22,18 @@ def SoftMax(x):
 
 
 def initialize_parameters(input_size,hidden_layer1_size,hidden_layer2_size, output_size):
-    # Initialize w small enough -> avoid 
+    """Initialize 
 
+    Args:
+        input_size (_type_): _description_
+        hidden_layer1_size (_type_): _description_
+        hidden_layer2_size (_type_): _description_
+        output_size (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
+    # Initialize w small enough -> avoid 
     weights = {
         'W1': np.random.randn(input_size, hidden_layer1_size) * 0.01,
         'b1': np.zeros((1, hidden_layer1_size)),
@@ -35,6 +46,16 @@ def initialize_parameters(input_size,hidden_layer1_size,hidden_layer2_size, outp
     return weights
 
 def forward_prop(X,weights,return_back = 'forward'):
+    """Forward_Propagation
+
+    Args:
+        X (_type_): _description_
+        weights (_type_): _description_
+        return_back (str, optional): _description_. Defaults to 'forward'.
+
+    Returns:
+        _type_: _description_
+    """    
     #Input Layer 
     Z1 = np.dot(X, weights['W1']) + weights['b1']  #X in shape(m, 28 x 28) -> W(28x28,hidden_layers_1)
     A1 = ReLU(Z1)
@@ -51,8 +72,89 @@ def forward_prop(X,weights,return_back = 'forward'):
     # Return type 2 when using prediction, since we'll only need the A3 results to predict
     elif return_back == 'predict':
         return A3
+    
+def compute_cost(AL,Y):
+    m = Y.shape[1] ## Or Shape [0] Depends on the Data Set.
 
-def backward_prop(...):
+    cost = -1/m * np.sum(Y * np.log(AL) + (1-Y) * np.log(1-AL)) 
+                         
+    cost = np.squeeze(cost)
+
+def backward_prop(X,Y,foward_data,weights):
+    """_summary_
+
+    Args:
+        X (_type_): _description_
+        Y (_type_): _description_
+        foward_data (_type_): _description_
+        weights (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """       
+    # Loads the results of the last forward propagation.
+    Z1, A1, Z2,A2, Z3,A3 = foward_data
+    # Number of samples in training.
+    m = X.shape[0] 
+
+    #Process
+    #Output Layer
+    dZ3 = A3 - Y # Derivative of cost regarding Z3 
+    gradient_W3 = np.dot(dZ3, A2.T) / m 
+    gradient_b3 = np.sum(dZ3, axis = 0, keepdims= True) / m
+
+    #Second Layer
+    dA2 = np.dot(dZ3,weights['W3'].T)
+    dZ2 = dA2 * ReLU_Deriv(Z2) 
+    gradient_W2 = np.dot(A1.T,dZ2) / m 
+    gradient_b2 = np.sum(dZ2, axis = 0, keepdims= True) / m 
+
+    # Input Layer 
+    dA1 = np.dot(dZ2,weights['W2'].T)
+    dZ1 = dA1 * ReLU_Deriv(Z1)
+    gradient_W1 = np.dot(X.T,dZ1)/m
+    gradient_b1 = np.sum(dZ1, axis = 0, keepdims= True)
+
+    # Save gradients as dictionary
+    gradients = {
+        'gradient_W3': gradient_W3, 'gradient_b3': gradient_b3,
+        'gradient_W2': gradient_W2, 'gradient_b2': gradient_b2,
+        'gradient_W1': gradient_W1, 'gradient_b1': gradient_b1
+    }
+    return gradients
+                 
+# Updates the weights using gradient descent
+def update_weights(weights, gradients, learning_rate):
+    """Update_Weights Gradient Descent
+
+    Args:
+        weights (_type_): _description_
+        gradients (_type_): _description_
+        learning_rate (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
+    weights['W3'] -= gradients['gradient_W3'] * learning_rate
+    weights['b3'] -= gradients['gradient_b3'] * learning_rate
+    weights['W2'] -= gradients['gradient_W2'] * learning_rate
+    weights['b2'] -= gradients['gradient_b2'] * learning_rate
+    weights['W1'] -= gradients['gradient_W1'] * learning_rate
+    weights['b1'] -= gradients['gradient_b1'] * learning_rate
+
+    return weights
+
+
+# Prediction Function 
+def predict(X,weights):
+    A3 = forward_prop(X,weights,return_back = 'predict')
+    return np.argmax(A3,axis = 1)
+
+def train():
+    pass 
+
+def
+
     
 
 
